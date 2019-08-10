97f92d45d1df718d01129a80702ef94fd8530a06
Safarali <mr.safarali@gmail.com>
2019-08-10 15:50:22 -0600

    Connect app to Mongo Atlas

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('DB connection successfull');
+    });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
