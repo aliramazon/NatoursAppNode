00ff9238367229d4348871d27750a95dde0a81eb
Safarali <mr.safarali@gmail.com>
2019-08-17 13:38:30 -0600

    Add /monthly-plan route using unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -111,6 +111,7 @@ exports.deleteTour = async (req, res) => {
     }
 };
 
+// tour-stats
 exports.getTourStats = async (req, res) => {
     try {
         const stats = await Tour.aggregate([
@@ -140,7 +141,58 @@ exports.getTourStats = async (req, res) => {
             }
         });
     } catch (err) {
-        req.status(404).json({
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+};
+
+// monthly-plan
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1; // 2021
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' }
+                }
+            },
+            {
+                $addFields: { month: '$_id' }
+            },
+            {
+                $project: { _id: 0 }
+            },
+            {
+                $sort: {
+                    numTourStarts: -1
+                }
+            }
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                plan
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
             status: 'fail',
             message: err
         });
