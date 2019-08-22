c80fae43c2b5d1e3ce2ff0ce07983c384b4b5529
Safarali <mr.safarali@gmail.com>
2019-08-21 22:50:40 -0600

    Handle unhandled routes

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Cant't find ${req.originalUrl} on this server`
+    });
+});
+
 module.exports = app;
