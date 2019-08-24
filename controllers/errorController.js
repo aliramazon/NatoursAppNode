787c9167c090c0b3ce982fcf40324d9f22a48694
Safarali <mr.safarali@gmail.com>
2019-08-23 22:38:01 -0600

    Hanle invalid database ID

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,10 @@
+const AppError = require('./../utils/appError');
+
 // Global Error Handler
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -37,6 +43,8 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = { ...err };
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        sendErrorProd(error, res);
     }
 };
