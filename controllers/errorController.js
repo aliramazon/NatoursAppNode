c50075fb9772419df7e0254ca2289f900b8697cd
Safarali <mr.safarali@gmail.com>
2019-08-23 22:06:14 -0600

    Seperate dev and prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,42 @@
 // Global Error Handler
 
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    // Else, Programming or other unknown error: dont leak error details
+
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        // 1) Log error
+        console.log('Error: ', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong'
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
 };
