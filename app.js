7c67f52f47dd32fd44e317080fed9a6441c3fa9c
Safarali <mr.safarali@gmail.com>
2019-09-02 21:13:51 -0600

    Protect from XSS and NoSQL query injecttion

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -16,7 +18,6 @@ const app = express();
 app.use(helmet());
 
 // Development logging
-
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -31,6 +32,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
