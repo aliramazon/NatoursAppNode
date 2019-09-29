b52d874b248f8436d106797d109a0866bdd72a05
Safarali <mr.safarali@gmail.com>
2019-09-29 01:40:00 -0600

    Crate Template for Overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find();
+    console.log(tours);
+    // 2) Build template
+
+    // 3) Render template using tour date from 1)
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All tours',
+        tours
     });
-};
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
