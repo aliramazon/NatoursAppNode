33e986d2564583a895ca76bae0be546954f934b7
Safarali <mr.safarali@gmail.com>
2019-09-29 21:10:25 -0600

    Show each tour details in seperate page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,18 +4,22 @@ const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res, next) => {
     // 1) Get tour data from collection
     const tours = await Tour.find();
-    console.log(tours);
-    // 2) Build template
 
-    // 3) Render template using tour date from 1)
+    // 2) Render template using tour date from 1)
     res.status(200).render('overview', {
         title: 'All tours',
         tours
     });
 });
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     });
-};
+
+    res.status(200).render('tour', {
+        title: tour.name,
+        tour: tour
+    });
+});
