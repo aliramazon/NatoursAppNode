b4d391562e97dcf5dc88bb9e808108eb5c98ccc5
Safarali <mr.safarali@gmail.com>
2019-10-19 23:48:14 -0600

    Enable CORS and change node version

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,10 +24,15 @@ const app = express();
 // Trust proxy. We need it for Heroku
 app.enable('trust proxy');
 
+// Views Settings
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Middlewares
+
+// CORS
+app.use(cors());
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
